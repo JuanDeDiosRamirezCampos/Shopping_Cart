a8dc53f706490cd56b684b1bf642e71245887041
JuanDeDiosRamirezCampos <juanmatacuicos@gmail.com>
2024-06-06 19:52:11 -0600

    Agrega funcionalidad de Login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../servicios/auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +19,9 @@ export class LoginComponent {
     ]]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private authService:AuthService,
+    private messageService: MessageService,
+    private router:Router) {}
 
   get email() {
     return this.LoginForm.controls['email'];
@@ -25,4 +30,23 @@ export class LoginComponent {
   get password() {
     return this.LoginForm.controls['password'];
   }
+  login() {
+    console.log('Login');
+    const { email, password } = this.LoginForm.value;
+  
+    this.authService.getUserByEmail(email as string).subscribe(
+      response => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/home']);
+        } else {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Email o Contraseña Incorrecta' });
+        }
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error en la autenticación' });
+      }
+    );
+  }
+  
 }
--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
 providedIn: 'root'
@@ -13,4 +14,7 @@ constructor(private http:HttpClient) {}
 registerUser (usuario: User) {
 return this.http.post(`${this.baseUrl}/users`, usuario);
 }
+getUserByEmail (email: String):Observable <User[]>{
+    return this.http.get<User[]>(`${this.baseUrl}/users?email=${email}`);
+    }
 }
